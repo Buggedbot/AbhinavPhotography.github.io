395a746d99fef47df0ec2685181f7523c81a7e05
Abhishek <70199385+Buggedbot@users.noreply.github.com>
2022-06-09 00:10:51 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,34 +28,25 @@
       }
     })
 
-    // Add smooth scrolling to all links
   $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
 
-      // Store hash
       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
     } // End if
   });
     
-  	// set lightbox img max height
   	const wHeight = $(window).height();
   	$(".lightbox-img").css("max-height",wHeight+"px");
 
-  	// lightbox 
   	$(".work-item-inner").click(function(){
   		index = $(this).parent(".work-item").index();
   		$(".lightbox").addClass("open");
@@ -80,12 +71,10 @@
   		lightboxSlideShow();
   	})
 
-  	// close lightbox 
   	$(".lightbox-close").click(function(){
   		$(".lightbox").removeClass("open");
   	})
 
-  	// close lightbox when clicked outside of img-box 
      $(".lightbox").click(function(event){
        if($(event.target).hasClass("lightbox")){
        	 $(this).removeClass("open");
